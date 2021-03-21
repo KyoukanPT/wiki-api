0f678ba643807e88a03838cdca40d5964dd1f7b4
KyoukanPT <59366068+KyoukanPT@users.noreply.github.com>
2021-03-21 03:35:37 -0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
